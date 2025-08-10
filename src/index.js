ac84715cdfa3aa808d2e491ae5a20fa15117aba6
mikedemianiuk <mikedteaches@gmail.com>
2025-08-10 19:38:01 -0400

    Complete US-02 randomness functions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const timerDisplay = document.querySelector('#timer');
 
 let time = 0;
 let timer;
-let lastHole = 0;
+let lastHole;
 let points = 0;
 let difficulty = "hard";
 
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -40,8 +40,9 @@ function randomInteger(min, max) {
  *
  */
 function setDelay(difficulty) {
-  // TODO: Write your code here.
-  
+  if (difficulty === "easy") return 1500;
+  if (difficulty === "normal") return 1000;
+  if (difficulty === "hard") return randomInteger(600, 1200);
 }
 
 /**
@@ -59,8 +60,11 @@ function setDelay(difficulty) {
  * chooseHole(holes) //> returns one of the 9 holes that you defined
  */
 function chooseHole(holes) {
-  // TODO: Write your code here.
-
+  const index = randomInteger(0, holes.length - 1);
+  const hole = holes[index];
+  if (hole === lastHole) return chooseHole(holes);
+  lastHole = hole;
+  return hole;
 }
 
 /**
